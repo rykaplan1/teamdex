698ce7f81c73370bf380fa8a0410be5d6c7934c6
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-27 11:53:13 -0500

    Added get route for all teams in the db

--- controllers/api/teamRoutes.js
+++ controllers/api/teamRoutes.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 // Change destructured object with whatever the model classes will be
 const { Team, Pokemon } = require('../../models');
 
-
+router.get('/', async (req, res) => {
+  try {
+    const dbTeamData = await Team.findAll({ include: [{ model: User, attributes: { exclude: 'password' } }] });
+    const teams = dbTeamData.map(team => team.get({ plain: true }));
+    res.status(200).json(teams)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
+});
 
 module.exports = router;

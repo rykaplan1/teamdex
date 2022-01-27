34678ea7f0db4babf9fcf345381fa25c65a99e8b
Michael Hranek <mhranek@comcast.net>
2022-01-27 13:45:14 -0500

    Updated Generation search to return array of pokemon names including previous generations

--- public/js/searchGeneration.js
+++ public/js/searchGeneration.js
@@ -1,14 +1,27 @@
 // Takes the user's input for which generation they want to pull from
+// const searchInput = document.getElementById('#search-input');
+// const searchBtn = document.getElementById('#search-btn');
 const pokeSearch = require('pokemon.js');
 pokeSearch.setLanguage('english');
 
-// TODO get user input from the search bar
-
 // TODO search for pokemon from a generation
 const searchGeneration = async function (num) {
-    // const 
-    const generation = await pokeSearch.getGeneration(3);
-    console.log(generation);
+    const pokemonArr = [];
+    // Gets all pokemon available to specified generation (including previous)
+    for (let i = num; i > 0; i--) {
+        const { pokemon_species } = await pokeSearch.getGeneration(num);
+        // Add all pokemon (by name) to pokemonArr
+        pokemon_species.forEach(pokemon => {
+            pokemonArr.push(pokemon);
+        });
+    }
+    return pokemonArr;
 }
 
-searchGeneration();
+// TODO when 'search' is clicked search a generation and display the pokemon availible to that generation to the page in a scroll-able section
+// searchBtn.addEventListener('click', async function () {
+// take user input to search for pokemon in a certain generation
+// const generationNum = searchInput.value;
+const pokeArr = searchGeneration(4);
+// });
+// searchGeneration();

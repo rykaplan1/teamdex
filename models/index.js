c12034f9307945d19d57dc12a2afe8efe5c629f9
Rich Kaplan <rykaplan1@gmail.com>
2022-01-27 11:56:23 -0500

    updated database name and added models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,23 @@
 const User = require('./User');
+const Team = require('./Team');
 const Pokemon = require('./Pokemon');
 
-User.hasMany(Pokemon, {
+User.hasMany(Team, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Pokemon.belongsTo(User, {
+Team.belongsTo(User, {
   foreignKey: 'user_id'
+})
+
+Team.hasMany(Pokemon, {
+  foreignKey: 'team_id',
+  onDelete: 'CASCADE'
+})
+
+Pokemon.belongsTo(Team, {
+  foreignKey: 'team_id'
 });
 
-module.exports = { User, Pokemon };
+module.exports = { User, Team, Pokemon };

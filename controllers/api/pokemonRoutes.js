481cee0dd69cd9fcceec90dee216cc7297c75ef0
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-27 11:44:56 -0500

    Initial commit

---
+++ controllers/api/pokemonRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+// Change destructured object with whatever the model classes will be
+const { Team, Pokemon } = require('../../models');
+
+
+
+module.exports = router;

727129ba3e31dcc60cf93ddda96c01d03980cf21
Michael Hranek <mhranek@comcast.net>
2022-01-27 14:36:18 -0500

    Updated generation search to interact with user input and button clicks

--- public/js/searchGeneration.js
+++ public/js/searchGeneration.js
@@ -1,11 +1,11 @@
 // Takes the user's input for which generation they want to pull from
-// const searchInput = document.getElementById('#search-input');
-// const searchBtn = document.getElementById('#search-btn');
+const searchInput = document.getElementById('#search-input'); // Drop down list for generations
+const searchBtn = document.getElementById('#search-btn'); // Search/submit button for searching a generation
 const pokeSearch = require('pokemon.js');
 pokeSearch.setLanguage('english');
 
-// TODO search for pokemon from a generation
-const searchGeneration = async function (num) {
+// Search for all pokemon from a generation
+const searchGeneration = async (num) => {
     const pokemonArr = [];
     // Gets all pokemon available to specified generation (including previous)
     for (let i = num; i > 0; i--) {
@@ -18,10 +18,17 @@ const searchGeneration = async function (num) {
     return pokemonArr;
 }
 
+// TODO Generate list of pokemon to the page
+const displayPokemonBulk = async function (pokemon) {
+    // create li for each pokemon in array, append to ol
+    // each element includes name, (is a) button to go to detailed page for that pokemon, ?Generation added?-stretch goal
+    console.log(pokemon);
+}
+
 // TODO when 'search' is clicked search a generation and display the pokemon availible to that generation to the page in a scroll-able section
-// searchBtn.addEventListener('click', async function () {
+searchBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
 // take user input to search for pokemon in a certain generation
-// const generationNum = searchInput.value;
-const pokeArr = searchGeneration(4);
-// });
-// searchGeneration();
+const pokeArr = await searchGeneration(searchInput.value);
+await displayPokemonBulk(pokeArr);
+});

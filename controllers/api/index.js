86f7f0a5d38ca3b0e97f5f224689b3bd10b0c8b3
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-27 13:24:02 -0500

    Added route to teamRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const teamRoutes = require('./teamRoutes');
 
 router.use('/users', userRoutes);
+router.use('/api', teamRoutes);
 
 module.exports = router;

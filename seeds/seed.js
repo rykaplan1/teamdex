28336a10e04fb9d9b9042d00728303a186967814
Rich Kaplan <rykaplan1@gmail.com>
2022-01-27 13:55:26 -0500

    created seed data and script

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Pokemon } = require('../models');
+const { User, Pokemon, Team } = require('../models');
 
 const userData = require('./userData.json');
+const teamData = require('./teamData.json');
 const pokemonData = require('./pokemonData.json');
 
 const seedDatabase = async () => {
@@ -12,7 +13,51 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  await Pokemon.bulkCreate(pokemonData);
+  await Team.Create({
+    ...teamData[0],
+    user_id: 0
+  });
+
+  for (let i = 0; i < 2; i++) {
+    await Pokemon.create({
+      ...pokemonData[i],
+      team_id: 0
+    });
+  }
+
+  await Team.Create({
+    ...teamData[1],
+    user_id: 2
+  });
+
+  for (let i = 2; i < 4; i++) {
+    await Pokemon.create({
+      ...pokemonData[i],
+      team_id: 1
+    });
+  }
+
+  for (let i = 2; i < teamData.length; i++) {
+    await Team.create({
+      ...teamData[i],
+      user_id: 1
+    })
+  }
+
+  let addedPokemon = 0;
+  let ashTeamId = 2;
+  for (let i = 4; i < pokemonData.length; i++) {
+    await Pokemon.create({
+      ...pokemonData[i],
+      team_id: ashTeamId
+    })
+    addedPokemon++;
+
+    if (addedPokemon === 6) {
+      addedPokemon = 0;
+      ashTeamId++;
+    }
+  }
 
   process.exit(0);
 };

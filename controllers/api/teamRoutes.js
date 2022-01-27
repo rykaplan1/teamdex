3c3b63fe0ec4a062d98986496a0dad78797cb86e
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-27 12:10:14 -0500

    Added POST route for adding a new team

--- controllers/api/teamRoutes.js
+++ controllers/api/teamRoutes.js
@@ -25,4 +25,21 @@ router.get('/:id', async (req, res) => {
   };
 });
 
+// POST for user to add team
+// TODO: Add withAuth after testing with insomnia
+router.post('/', async (req, res) => {
+  try {
+    const newTeam = await Team.create({
+      team_name: req.body.team_name,
+      game: req.body.game,
+      num_pokemon: req.body.num_pokemon,
+      user_id: req.session.userId
+    });
+    res.status(200).json(newTeam);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
+});
+
 module.exports = router;

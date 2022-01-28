62dd6efc299c341bff1369d6ecb7639ff3c57334
Michael Hranek <mhranek@comcast.net>
2022-01-28 11:39:19 -0500

    Updated fetch request to pokeAPI

--- public/js/searchGeneration.js
+++ public/js/searchGeneration.js
@@ -1,23 +1,23 @@
 // Takes the user's input for which generation they want to pull from
-const searchInput = document.getElementById('#pokemon-search'); // Drop down list for generations
-const searchBtn = document.getElementById('#search-btn'); // Search/submit button for searching a generation
+const searchInput = document.getElementById('pokemon-search'); // Drop down list for generations
+const searchBtn = document.getElementById('search-btn'); // Search/submit button for searching a generation
 // const pokeSearch = require('pokemon.js');
 // pokeSearch.setLanguage('english');
-const pokeAPIURL = 'https://pokeapi.co/api/v2/generation/';
+let pokeAPIURL = 'https://pokeapi.co/api/v2/generation/';
 
 // Search for all pokemon from a generation
 const searchGeneration = async (num) => {
     const pokemonArr = [];
     // Gets all pokemon available to specified generation (including previous)
-    for (let i = num; i > 0; i--) {
-        pokeAPIURL += num;
-        const pokemonAPI = await fetch(pokeAPIURL);
-        // Add all pokemon (by name) to pokemonArr
-        pokemon_species.forEach(pokemon => {
-            pokemonArr.push(pokemon);
-        });
-    }
-    return pokemonArr;
+    // for (let i = num; i > 0; i--) {
+    pokeAPIURL += num;
+    const pokeData = await fetch(pokeAPIURL);
+    console.log(pokeData);
+    // Add all pokemon (by name) to pokemonArr
+    // await pokemon_species.forEach(pokemon => {
+    //     pokemonArr.push(pokemon);
+    // });
+    // }
 }
 
 // TODO Generate list of pokemon to the page
@@ -28,14 +28,9 @@ const displayPokemonBulk = async function (pokemon) {
 }
 
 // TODO when 'search' is clicked search a generation and display the pokemon availible to that generation to the page in a scroll-able section
-if (searchBtn) {
-    document.searchBtn.addEventListener('submit', (event) => {
-        event.preventDefault();
-        console.log("You clicked submit");
-        // take user input to search for pokemon in a certain generation
-        const pokeArr = searchGeneration(searchInput.value);
-        displayPokemonBulk(pokeArr);
-    });
-} else {
-    console.log("SearchBtn is null");
-}
+searchBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    // take user input to search for pokemon in a certain generation
+    const pokeArr = searchGeneration(searchInput.value);
+    // displayPokemonBulk(pokeArr);
+});

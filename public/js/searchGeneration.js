76ca91f44ba42a0bbfac79159603f81e99e110ad
Michael Hranek <mhranek@comcast.net>
2022-01-28 11:07:21 -0500

    Committing testing html to pull

--- public/js/searchGeneration.js
+++ public/js/searchGeneration.js
@@ -1,15 +1,17 @@
 // Takes the user's input for which generation they want to pull from
-const searchInput = document.getElementById('#search-input'); // Drop down list for generations
+const searchInput = document.getElementById('#pokemon-search'); // Drop down list for generations
 const searchBtn = document.getElementById('#search-btn'); // Search/submit button for searching a generation
-const pokeSearch = require('pokemon.js');
-pokeSearch.setLanguage('english');
+// const pokeSearch = require('pokemon.js');
+// pokeSearch.setLanguage('english');
+const pokeAPIURL = 'https://pokeapi.co/api/v2/generation/';
 
 // Search for all pokemon from a generation
 const searchGeneration = async (num) => {
     const pokemonArr = [];
     // Gets all pokemon available to specified generation (including previous)
     for (let i = num; i > 0; i--) {
-        const { pokemon_species } = await pokeSearch.getGeneration(num);
+        pokeAPIURL += num;
+        const pokemonAPI = await fetch(pokeAPIURL);
         // Add all pokemon (by name) to pokemonArr
         pokemon_species.forEach(pokemon => {
             pokemonArr.push(pokemon);
@@ -26,9 +28,14 @@ const displayPokemonBulk = async function (pokemon) {
 }
 
 // TODO when 'search' is clicked search a generation and display the pokemon availible to that generation to the page in a scroll-able section
-searchBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
-// take user input to search for pokemon in a certain generation
-const pokeArr = await searchGeneration(searchInput.value);
-await displayPokemonBulk(pokeArr);
-});
+if (searchBtn) {
+    document.searchBtn.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log("You clicked submit");
+        // take user input to search for pokemon in a certain generation
+        const pokeArr = searchGeneration(searchInput.value);
+        displayPokemonBulk(pokeArr);
+    });
+} else {
+    console.log("SearchBtn is null");
+}

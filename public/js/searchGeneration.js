3f62c41c56f8872993f79da2dd71b126d151f694
Michael Hranek <mhranek@comcast.net>
2022-01-28 13:26:42 -0500

    Committing for pulling

--- public/js/searchGeneration.js
+++ public/js/searchGeneration.js
@@ -22,12 +22,13 @@ const searchGeneration = async (num) => {
 
 // TODO Generate list of pokemon to the page
 const displayPokemonBulk = async function (pokemon) {
-    // create li for each pokemon in array, append to ol
+    // create drop down list of pokemon names
+    
     // each element includes name, (is a) button to go to detailed page for that pokemon, ?Generation added?-stretch goal
     console.log(pokemon);
 }
 
-// TODO when 'search' is clicked search a generation and display the pokemon availible to that generation to the page in a scroll-able section
+// when 'search' is clicked search a generation and display the pokemon availible to that generation to the page in a scroll-able section
 searchBtn.addEventListener('click', async (event) => {
     event.preventDefault();
     // take user input to search for pokemon in a certain generation

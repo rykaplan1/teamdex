20be11b3ffec0d2b40e5d9f6ab01c314c425081e
Michael Hranek <mhranek@comcast.net>
2022-01-30 16:38:36 -0500

    Populating game list with all games that will return info from the API

--- public/js/searchGeneration.js
+++ public/js/searchGeneration.js
@@ -2,6 +2,78 @@
 const searchInput = document.getElementById('pokemon-search'); // Drop down list for generations
 const searchBtn = document.getElementById('search-btn'); // Search/submit button for searching a generation
 const dataList = document.getElementById('pokemon-list');
+const gameList = document.getElementById('game-list');
+
+const gameListData = [
+    {
+        name: 'Red-Blue',
+        generation: 1
+    },
+    {
+        name: 'Yellow',
+        generation: 1
+    },
+    {
+        name: 'Gold-Silver',
+        generation: 2
+    },
+    {
+        name: 'Crystal',
+        generation: 2
+    },
+    {
+        name: 'Ruby-Sapphire',
+        generation: 3
+    },
+    {
+        name: 'Emerald',
+        generation: 3
+    },
+    {
+        name: 'Firered-Leafgreen',
+        generation: 3
+    },
+    {
+        name: 'Diamond-Pearl',
+        generation: 4
+    },
+    {
+        name: 'Platinum',
+        generation: 4
+    },
+    {
+        name: 'Heartgold-Soulsilver',
+        generation: 4
+    },
+    {
+        name: 'Black-White',
+        generation: 5
+    },
+    {
+        name: 'Black 2-White 2',
+        generation: 5
+    },
+    {
+        name: 'X-Y',
+        generation: 6
+    },
+    {
+        name: 'Omeaga Ruby-Alpha Sapphire',
+        generation: 6
+    },
+    {
+        name: 'Sun-Moon',
+        generation: 7
+    },
+    {
+        name: 'Ultra Sun-Ultra Moon',
+        generation: 7
+    },
+    {
+        name: 'Sword-Shield',
+        generation: 8
+    }
+]
 
 // Search for all pokemon from a generation
 const searchGeneration = async (num) => {
@@ -30,6 +102,17 @@ const displayPokemonBulk = async function (pokemon) {
     })
 }
 
+// Generate list of games (data value for the generation)
+const generateGamesList = function () {
+    gameListData.forEach(game => {
+        const listEntry = document.createElement('option');
+        listEntry.innerHTML = game.name;
+        listEntry.setAttribute('data-generation', game.generation);
+        gameList.append(listEntry);
+    });
+    console.log(gameList);
+}
+
 // when 'search' is clicked search a generation and display the pokemon availible to that generation to the page in a scroll-able section
 searchBtn.addEventListener('click', async (event) => {
     event.preventDefault();
@@ -37,3 +120,5 @@ searchBtn.addEventListener('click', async (event) => {
     const pokeArr = await searchGeneration(searchInput.value);
     displayPokemonBulk(pokeArr);
 });
+
+generateGamesList();
